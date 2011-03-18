b3468c9099b3229b37828f653b2d3aa74f764ddf
kim_rutherford <kim_rutherford@94fbd4ce-d3a6-4fcd-866a-3d22ea184563>
2011-03-18 13:35:06 +0000

    Added an exporter for phenotypes
    git-svn-id: https://fission-yeast.svn.sourceforge.net/svnroot/fission-yeast/chado/trunk@107 94fbd4ce-d3a6-4fcd-866a-3d22ea184563

---
+++ lib/PomBase/Retrieve/Phenotypes.pm
@@ -0,0 +1,64 @@
+package PomBase::Retrieve::Phenotypes;
+
+=head1 NAME
+
+PomBase::Retrieve::Phenotypes - Retrieve phenotypes to a file
+
+=head1 SYNOPSIS
+
+=head1 AUTHOR
+
+Kim Rutherford C<< <kmr44@cam.ac.uk> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<kmr44@cam.ac.uk>.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc PomBase::Retrieve::Phenotypes
+
+=over 4
+
+=back
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Kim Rutherford, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=head1 FUNCTIONS
+
+=cut
+
+use perl5i::2;
+use Moose;
+
+use List::Gen 'iterate';
+
+method retrieve() {
+  my $chado = $self->chado();
+
+  my $it = do {
+    my $results =
+      $chado->resultset('Cv::Cv')->search({ 'me.name' => 'phenotype' })
+        ->search_related('cvterms');
+
+    iterate {
+      my $row = $results->next();
+      if (defined $row) {
+        my $dbxref = $row->dbxref();
+        my $id = $dbxref->db()->name() . ':' . $dbxref->accession();
+        return [$row->name(), $id];
+      } else {
+        return undef;
+      }
+    };
+  };
+}
+
+with 'PomBase::Retriever';
---
+++ pombase-export.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/perl -w
+
+use perl5i::2;
+
+use YAML qw(LoadFile);
+
+BEGIN {
+  push @INC, 'lib';
+};
+
+if (@ARGV != 5) {
+  die qq($0: needs fives arguments:
+  retrieve_type - currently only "phenotypes"
+  config_file   - the YAML format configuration file name
+  database_name - the Chado database name
+  username      - the database user name
+  password      - the database password
+);
+}
+
+my $retrieve_type = shift;
+my $config_file = shift;
+my $database = shift;
+my $username = shift;
+my $password = shift;
+
+use PomBase::Chado;
+my $chado = PomBase::Chado::connect($database, $username, $password);
+
+my $config = LoadFile($config_file);
+
+my %retrieve_modules = (
+  phenotypes => 'PomBase::Retrieve::Phenotypes',
+);
+
+my $retrieve_module = $retrieve_modules{$retrieve_type};
+
+if (defined $retrieve_module) {
+  my $retriever =
+    eval qq{
+require $retrieve_module;
+$retrieve_module->new(chado => \$chado, config => \$config);
+};
+  die "$@" if $@;
+
+  my $results = $retriever->retrieve();
+
+  while (my $row = $results->next()) {
+    say join "\t", @$row;
+  }
+} else {
+  die "unknown type to retrieve: $retrieve_type\n";
+}
