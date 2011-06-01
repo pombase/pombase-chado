d7a7fe28ffd3675950a98b7b1125b97dfed13c26
Kim Rutherford <kmr44@cam.ac.uk>
2011-06-01 13:52:26 +0100

    Add script for making test databases

---
+++ etc/make-test-db.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl -w
+
+use perl5i::2;
+use Moose;
+
+my $max = -1;
+
+open my $psql, 'psql -l|' or die;
+
+while (defined (my $line = <$psql>)) {
+  if ($line =~ /load-test-(\d+)/) {
+    if ($1 > $max) {
+      $max = $1;
+    }
+  }
+}
+
+close $psql;
+
+my $new_num = $max + 1;
+
+my $new_db_name = "load-test-$new_num";
+
+print "creating new database\n";
+
+system "createdb -T load-test-template $new_db_name";
+
+use IO::All;
+
+$new_db_name > io('/tmp/new_test_db');
+
+print "new database name: $new_db_name\n";
