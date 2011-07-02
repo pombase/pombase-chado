a2a13d709585c1510fa7e37e043be6583d672e54
Kim Rutherford <kmr44@cam.ac.uk>
2011-07-02 08:57:11 +0100

    Fix uninitialised variable

--- lib/PomBase/Chado/QualifierLoad.pm
+++ lib/PomBase/Chado/QualifierLoad.pm
@@ -295,7 +295,7 @@ method add_term_to_gene($pombe_feature, $cv_name, $embl_term_name, $sub_qual_map
           my $db_term_id = $db->name() . ":" . $dbxref->accession();
           my $embl_cvterm =
             $self->find_cvterm_by_term_id($qualifier_term_id);
-          if ($db_term_id eq $obsolete_id) {
+          if (defined $obsolete_id && $db_term_id eq $obsolete_id) {
             # use the cvterm we got from the GOid, not the name
             $cvterm = $embl_cvterm;
           } else {
