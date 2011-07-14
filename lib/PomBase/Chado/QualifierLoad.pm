396e1f583cb3b7d717d4711ccf2852b8b69a1715
Kim Rutherford <kmr44@cam.ac.uk>
2011-07-14 07:50:58 +0100

    Catch case when both ID and name are unknown

--- lib/PomBase/Chado/QualifierLoad.pm
+++ lib/PomBase/Chado/QualifierLoad.pm
@@ -269,6 +269,9 @@ method add_term_to_gene($pombe_feature, $cv_name, $embl_term_name, $sub_qual_map
       }
       if (!defined $cvterm) {
         $cvterm = $self->find_cvterm_by_term_id($qualifier_term_id);
+        if (!defined $cvterm) {
+          die qq(unknown term name "$embl_term_name" and unknown GO ID "$qualifier_term_id"\n);
+        }
         if (!$self->config()->{allowed_unknown_term_names}->{$qualifier_term_id}) {
           die "found cvterm by ID, but name doesn't match any cvterm: $qualifier_term_id " .
             "EMBL file: $embl_term_name  Chado name for ID: ", $cvterm->name(), "\n";
