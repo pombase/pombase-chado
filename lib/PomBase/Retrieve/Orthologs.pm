5d1ca51fb0750a98f386961d0c9e747f77debbb9
Kim Rutherford <kmr44@cam.ac.uk>
2011-07-18 18:15:47 +0100

    Finished ortholog export code

--- lib/PomBase/Retrieve/Orthologs.pm
+++ lib/PomBase/Retrieve/Orthologs.pm
@@ -46,21 +46,59 @@ with 'PomBase::Role::CvQuery';
 method retrieve() {
   my $chado = $self->chado();
 
-  my $it = do {
-    my $orthologous_to_cvterm = $self->get_cvterm('sequence', 'orthologous_to');
+  my $dbh = $self->chado()->storage()->dbh();
 
-    my $results =
-      $chado->resultset('Sequence::FeatureRelationship')
-        ->search({ 'me.type_id' => $orthologous_to_cvterm->cvterm_id() },
-                 { prefetch => [ 'subject', 'object' ],
-                   order_by => 'subject_id' });
+  my $protein_temp = "
+CREATE TEMP TABLE protein_coding_genes AS
+SELECT object_id
+FROM feature_relationship r, cvterm rt, feature s, cvterm st
+WHERE r.subject_id = s.feature_id
+  AND r.type_id = rt.cvterm_id
+  AND s.type_id = st.cvterm_id
+  AND st.name = 'mRNA';";
+
+  my $ortholog_temp = "
+CREATE TEMP TABLE ortholog_list AS
+SELECT object.uniquename as o_un, subject.name as s_name
+FROM feature_relationship me
+JOIN feature subject ON subject.feature_id = me.subject_id
+JOIN feature object ON object.feature_id = me.object_id
+WHERE me.type_id = (select cvterm_id from cvterm where name = 'orthologous_to')
+  AND subject.organism_id = (select organism_id from organism where common_name = 'Scerevisiae')
+  AND object.feature_id in (select * from protein_coding_genes)
+UNION
+SELECT uniquename as o_un, 'NONE' as s_name
+ FROM feature f where f.feature_id not in
+      (select object_id from feature_relationship r, feature subject_f
+        WHERE r.subject_id = subject_f.feature_id
+          AND subject_f.organism_id = (select organism_id from organism where
+        common_name = 'Scerevisiae'))
+  AND f.organism_id = (select organism_id from organism where
+        common_name = 'pombe')
+  AND f.type_id = (select cvterm_id from cvterm where name = 'gene')
+  AND f.feature_id in (select * from protein_coding_genes)
+ ORDER BY o_un, s_name;";
+
+  my $query = "
+SELECT o_un, string_agg(s_name, '|')
+FROM ortholog_list
+GROUP BY o_un
+ ORDER BY o_un;";
+
+  my $it = do {
+    my $sth = $dbh->prepare($protein_temp);
+    $sth->execute() or die "Couldn't execute: " . $sth->errstr;
+
+    $sth = $dbh->prepare($ortholog_temp);
+    $sth->execute() or die "Couldn't execute: " . $sth->errstr;
+
+    $sth = $dbh->prepare($query);
+    $sth->execute() or die "Couldn't execute: " . $sth->errstr;
 
     iterate {
-      my $row = $results->next();
-      if (defined $row) {
-        my $subject = $row->subject();
-        my $object = $row->object();
-        return [$subject->uniquename(), $object->uniquename()];
+      my @data = $sth->fetchrow_array();
+      if (@data) {
+        return [@data];
       } else {
         return undef;
       }
