eba827d1f6abb61171fc0dad55cdd3ee40d4dbc2
Kim Rutherford <kmr44@cam.ac.uk>
2011-11-12 12:47:44 +0000

    Fixed database location

--- etc/load-all.sh
+++ etc/load-all.sh
@@ -16,13 +16,13 @@ $HOME/git/pombase-run/script/load-chado.pl -d --mapping "pt_mod:PSI-MOD:$HOME/Dr
 $HOME/git/pombase-run/etc/process-log.pl $log_file
 
 cd $HOME/git/pombase-run
-./script/pombase-import.pl ./load-chado.yaml biogrid sloth $DB kmr44 kmr44 < $HOME/downloads/BIOGRID-ORGANISM-Schizosaccharomyces_pombe-3.1.78.tab2.txt
+./script/pombase-import.pl ./load-chado.yaml biogrid oliver0 $DB kmr44 kmr44 < $HOME/downloads/BIOGRID-ORGANISM-Schizosaccharomyces_pombe-3.1.78.tab2.txt
 
-./script/pombase-import.pl ./load-chado.yaml gaf --assigned-by-filter=GeneDB_Spombe sloth pombe-chado-ng-v13-full-goa kmr44 kmr44 < $HOME/Work/pombe/pombe-embl/external-go-data/go_comp.tex
-./script/pombase-import.pl ./load-chado.yaml gaf --assigned-by-filter=GeneDB_Spombe sloth pombe-chado-ng-v13-full-goa kmr44 kmr44 < $HOME/Work/pombe/pombe-embl/external-go-data/go_proc.tex
-./script/pombase-import.pl ./load-chado.yaml gaf --assigned-by-filter=GeneDB_Spombe sloth pombe-chado-ng-v13-full-goa kmr44 kmr44 < $HOME/Work/pombe/pombe-embl/external-go-data/go_func.tex
-./script/pombase-import.pl ./load-chado.yaml gaf --term-id-filter-filename=~kmr44/pombe/pombe-embl/goa-load-fixes/filtered_GO_IDs --db-ref-filter-filename=~kmr44/pombe/pombe-embl/goa-load-fixes/filtered_mappings --assigned-by-filter=GeneDB_Spombe sloth pombe-chado-ng-v13-full-goa kmr44 kmr44 < $HOME/Work/pombe/sources/gene_association.GeneDB_Spombe.inf.gaf
-./script/pombase-import.pl ./load-chado.yaml gaf --assigned-by-filter=GeneDB_Spombe sloth pombe-chado-ng-v13-full-goa kmr44 kmr44 < $HOME/Work/pombe/pombe-embl/external-go-data/From_curation_tool
-./script/pombase-import.pl ./load-chado.yaml gaf --assigned-by-filter=GeneDB_Spombe sloth pombe-chado-ng-v13-full-goa kmr44 kmr44 < $HOME/Work/pombe/pombe-embl/external-go-data/GO_ORFeome_localizations2.tex
-./script/pombase-import.pl ./load-chado.yaml gaf --term-id-filter-filename=~kmr44/pombe/pombe-embl/goa-load-fixes/filtered_GO_IDs --db-ref-filter-filename=~kmr44/pombe/pombe-embl/goa-load-fixes/filtered_mappings --assigned-by-filter=InterPro,UniProtKB sloth pombe-chado-ng-v13-full-goa kmr44 kmr44 < ~/Work/pombe/gene_association.goa_uniprot.pombe
+./script/pombase-import.pl ./load-chado.yaml gaf --assigned-by-filter=GeneDB_Spombe oliver0 pombe-chado-ng-v13-full-goa kmr44 kmr44 < $HOME/Work/pombe/pombe-embl/external-go-data/go_comp.tex
+./script/pombase-import.pl ./load-chado.yaml gaf --assigned-by-filter=GeneDB_Spombe oliver0 pombe-chado-ng-v13-full-goa kmr44 kmr44 < $HOME/Work/pombe/pombe-embl/external-go-data/go_proc.tex
+./script/pombase-import.pl ./load-chado.yaml gaf --assigned-by-filter=GeneDB_Spombe oliver0 pombe-chado-ng-v13-full-goa kmr44 kmr44 < $HOME/Work/pombe/pombe-embl/external-go-data/go_func.tex
+./script/pombase-import.pl ./load-chado.yaml gaf --term-id-filter-filename=~kmr44/pombe/pombe-embl/goa-load-fixes/filtered_GO_IDs --db-ref-filter-filename=~kmr44/pombe/pombe-embl/goa-load-fixes/filtered_mappings --assigned-by-filter=GeneDB_Spombe oliver0 pombe-chado-ng-v13-full-goa kmr44 kmr44 < $HOME/Work/pombe/sources/gene_association.GeneDB_Spombe.inf.gaf
+./script/pombase-import.pl ./load-chado.yaml gaf --assigned-by-filter=GeneDB_Spombe oliver0 pombe-chado-ng-v13-full-goa kmr44 kmr44 < $HOME/Work/pombe/pombe-embl/external-go-data/From_curation_tool
+./script/pombase-import.pl ./load-chado.yaml gaf --assigned-by-filter=GeneDB_Spombe oliver0 pombe-chado-ng-v13-full-goa kmr44 kmr44 < $HOME/Work/pombe/pombe-embl/external-go-data/GO_ORFeome_localizations2.tex
+./script/pombase-import.pl ./load-chado.yaml gaf --term-id-filter-filename=~kmr44/pombe/pombe-embl/goa-load-fixes/filtered_GO_IDs --db-ref-filter-filename=~kmr44/pombe/pombe-embl/goa-load-fixes/filtered_mappings --assigned-by-filter=InterPro,UniProtKB oliver0 pombe-chado-ng-v13-full-goa kmr44 kmr44 < ~/Work/pombe/gene_association.goa_uniprot.pombe
 ./script/filter-redundant-go.sh
