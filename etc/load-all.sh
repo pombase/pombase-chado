87c0045a9a6803797be5c30eff2c1b7ec495f945
Kim Rutherford <kmr44@cam.ac.uk>
2011-11-15 02:30:16 +0000

    Added debug messages

--- etc/load-all.sh
+++ etc/load-all.sh
@@ -18,8 +18,12 @@ log_file=log.`date_string`
 $HOME/git/pombase-run/script/load-chado.pl -d --mapping "pt_mod:PSI-MOD:$HOME/Dropbox/pombase/chado_load_warnings/modification_map.txt" --mapping "phenotype:fission_yeast_phenotype:$HOME/Dropbox/pombase/phenotype/phenotype-map.txt" --obsolete-term-map $HOME/pombe/go-doc/obsoletes-exact $HOME/git/pombase-run/load-chado.yaml $HOST $DB $USER $PASSWORD $HOME/pombe/pombe-embl/*.contig 2>&1 | tee $log_file
 $HOME/git/pombase-run/etc/process-log.pl $log_file
 
+echo starting import of biogrid data 1>&2
+
 cd $HOME/git/pombase-run
 ./script/pombase-import.pl ./load-chado.yaml biogrid oliver0 $DB kmr44 kmr44 < $HOME/downloads/BIOGRID-ORGANISM-Schizosaccharomyces_pombe-3.1.78.tab2.txt
+
+echo starting import of GOA GAF data 1>&2
 
 ./script/pombase-import.pl ./load-chado.yaml gaf --assigned-by-filter=GeneDB_Spombe oliver0 pombe-chado-ng-v13-full-goa kmr44 kmr44 < $HOME/Work/pombe/pombe-embl/external-go-data/go_comp.tex
 ./script/pombase-import.pl ./load-chado.yaml gaf --assigned-by-filter=GeneDB_Spombe oliver0 pombe-chado-ng-v13-full-goa kmr44 kmr44 < $HOME/Work/pombe/pombe-embl/external-go-data/go_proc.tex
