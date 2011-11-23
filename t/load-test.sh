520f3d3811ec29009b773174e23e77880598d454
Kim Rutherford <kmr44@cam.ac.uk>
2011-11-23 04:56:55 +0000

    Added script to load a small test dataset

---
+++ t/load-test.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#CARP=-MCarp::Always
+CARP=
+
+cd data
+DBIC_TRACE=0 PERL5LIB=../lib time perl $CARP ../script/load-chado.pl -d -t \
+  --mapping "pt_mod:PSI-MOD:$HOME/Dropbox/pombase/ontologies/PSI-MOD/modification_map.txt" \
+  --mapping "phenotype:fission_yeast_phenotype:$HOME/Dropbox/pombase/ontologies/phenotype/phenotype-map.txt" \
+  --obsolete-term-map ~/pombe/go-doc/obsoletes-exact ../load-chado.yaml localhost `cat /tmp/new_test_db` kmr44 kmr44 chromosome1.contig.embl 2>&1 | cut -c1-300
