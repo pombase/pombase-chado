317181b8a8f0d079f64f4c013cfe0a0606dc38c5
Kim Rutherford <kmr44@cam.ac.uk>
2011-11-24 03:05:20 +0000

    Don't ignore DBIC trace flag

--- t/load-test.sh
+++ t/load-test.sh
@@ -4,7 +4,7 @@
 CARP=
 
 cd data
-DBIC_TRACE=0 PERL5LIB=../lib time perl $CARP ../script/load-chado.pl -d -t \
+PERL5LIB=../lib time perl $CARP ../script/load-chado.pl -d -t -v \
   --mapping "pt_mod:PSI-MOD:$HOME/Dropbox/pombase/ontologies/PSI-MOD/modification_map.txt" \
   --mapping "phenotype:fission_yeast_phenotype:$HOME/Dropbox/pombase/ontologies/phenotype/phenotype-map.txt" \
   --obsolete-term-map ~/pombe/go-doc/obsoletes-exact ../load-chado.yaml localhost `cat /tmp/new_test_db` kmr44 kmr44 chromosome1.contig.embl 2>&1 | cut -c1-300
