e664dec387833e59d7dc4f0c1499de49c8a9f5c4
Kim Rutherford <kmr44@cam.ac.uk>
2011-11-25 18:32:49 +0000

    Add protein sequence feature to SO mapping to load script

--- etc/load-all.sh
+++ etc/load-all.sh
@@ -17,7 +17,12 @@ export PERL5LIB=$HOME/git/pombase-run/lib
 
 cd $HOME/chado/embl_load
 log_file=log.`date_string`
-$HOME/git/pombase-run/script/load-chado.pl --mapping "pt_mod:PSI-MOD:$HOME/Dropbox/pombase/ontologies/PSI-MOD/modification_map.txt" --mapping "phenotype:fission_yeast_phenotype:$HOME/Dropbox/pombase/ontologies/phenotype/phenotype-map.txt" --obsolete-term-map $HOME/pombe/go-doc/obsoletes-exact $HOME/git/pombase-run/load-chado.yaml $HOST $DB $USER $PASSWORD $HOME/pombe/pombe-embl/*.contig 2>&1 | tee $log_file
+$HOME/git/pombase-run/script/load-chado.pl \
+  --mapping "sequence_feature:sequence:$HOME/Dropbox/pombase/ontologies/SO/features-to-so_mapping_only.txt" \
+  --mapping "pt_mod:PSI-MOD:$HOME/Dropbox/pombase/ontologies/PSI-MOD/modification_map.txt" \
+  --mapping "phenotype:fission_yeast_phenotype:$HOME/Dropbox/pombase/ontologies/phenotype/phenotype-map.txt" \
+  --obsolete-term-map $HOME/pombe/go-doc/obsoletes-exact $HOME/git/pombase-run/load-chado.yaml \
+  $HOST $DB $USER $PASSWORD $HOME/pombe/pombe-embl/*.contig 2>&1 | tee $log_file
 $HOME/git/pombase-run/etc/process-log.pl $log_file
 
 echo starting import of biogrid data 1>&2
