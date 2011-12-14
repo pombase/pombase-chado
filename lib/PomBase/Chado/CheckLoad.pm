ca014152d0a8ef6a9d24866d3999e4ebeeeed44c
Kim Rutherford <kmr44@cam.ac.uk>
2011-12-14 14:36:22 +0000

    Add an example of a column_17 qualifier

--- lib/PomBase/Chado/CheckLoad.pm
+++ lib/PomBase/Chado/CheckLoad.pm
@@ -129,7 +129,7 @@ method check
   my $coiled_coil_cvterm = $self->get_cvterm('sequence', 'coiled_coil');
 
   my @all_feature_cvterm = $chado->resultset('Sequence::FeatureCvterm')->all();
-  should(scalar(@all_feature_cvterm), 118);
+  should(scalar(@all_feature_cvterm), 119);
 
   my $feature_cvterm_rs =
     $transcript->feature_cvterms()->search({
@@ -152,7 +152,7 @@ method check
   should(scalar(@props), 4);
 
   my @all_props = $chado->resultset('Sequence::FeatureCvtermprop')->all();
-  should(scalar(@all_props), 185);
+  should(scalar(@all_props), 187);
 
   my $feat_rs = $chado->resultset('Sequence::Feature');
   should ($feat_rs->count(), 71);
