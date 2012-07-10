6e3cbb7b61b053e3c89700cd7d562702c3804525
Kim Rutherford <kmr44@cam.ac.uk>
2012-07-10 01:14:45 +0100

    Add warnings for some phenotype extension rels

--- lib/PomBase/Chado/ExtensionProcessor.pm
+++ lib/PomBase/Chado/ExtensionProcessor.pm
@@ -121,6 +121,16 @@ method store_extension($feature_cvterm, $extensions)
 
       if (defined $term) {
         my $rel = $self->find_cvterm_by_name($relationship_cv_name, $rel_name);
+        my $old_cv_name = $old_cvterm->cv()->name();
+        my $restrictions_conf = $self->config()->{extension_restrictions}->{$old_cv_name};
+
+        if (defined $restrictions_conf) {
+          if (exists $restrictions_conf->{allowed}) {
+            if (!grep { $_ eq $rel_name } @{$restrictions_conf->{allowed}}) {
+              die "$rel_name() not allowed for $old_cv_name\n";
+            }
+          }
+        }
 
         if (!defined $rel) {
           die "can't find relation cvterm for: $rel_name\n";
