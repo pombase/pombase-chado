7b203f8b83a76302ecd8312d6337d002413a0406
Kim Rutherford <kmr44@cam.ac.uk>
2012-08-14 03:40:08 +0100

    Reduce the hard-coding of paths

--- etc/load-all.sh
+++ etc/load-all.sh
@@ -9,7 +9,9 @@ PASSWORD=$4
 
 LOG_DIR=`pwd`
 
-cd /var/pomcur/sources/pombe-embl/
+SOURCES=/var/pomcur/sources
+
+cd $SOURCES/pombe-embl/
 svn update || exit 1
 
 cd $HOME/git/pombase-run
@@ -24,12 +26,12 @@ $HOME/git/pombase-run/script/load-chado.pl \
   --mapping "pt_mod:PSI-MOD:$HOME/Dropbox/pombase/ontologies/PSI-MOD/modification_map.txt" \
   --mapping "phenotype:fission_yeast_phenotype:$HOME/Dropbox/pombase/ontologies/phenotype/phenotype-map.txt" \
   --obsolete-term-map $HOME/pombe/go-doc/obsoletes-exact $HOME/git/pombase-run/load-chado.yaml \
-  $HOST $DB $USER $PASSWORD /var/pomcur/sources/pombe-embl/*.contig 2>&1 | tee $log_file
+  $HOST $DB $USER $PASSWORD $SOURCES/pombe-embl/*.contig 2>&1 | tee $log_file
 $HOME/git/pombase-run/etc/process-log.pl $log_file
 
 echo starting import of biogrid data | tee $log_file.biogrid
 
-(cd /var/pomcur/sources/biogrid
+(cd $SOURCES/biogrid
 mv BIOGRID-* old/
 wget http://thebiogrid.org/downloads/archives/Latest%20Release/BIOGRID-ORGANISM-LATEST.tab2.zip
 unzip -q BIOGRID-ORGANISM-LATEST.tab2.zip
@@ -41,7 +43,7 @@ fi
 ) 2>&1 | tee -a $log_file.biogrid
 
 cd $HOME/git/pombase-run
-cat /var/pomcur/sources/biogrid/BIOGRID-ORGANISM-Schizosaccharomyces_pombe-*.tab2.txt | ./script/pombase-import.pl ./load-chado.yaml biogrid $HOST $DB $USER $PASSWORD 2>&1 | tee -a $LOG_DIR/$log_file.biogrid
+cat $SOURCES/biogrid/BIOGRID-ORGANISM-Schizosaccharomyces_pombe-*.tab2.txt | ./script/pombase-import.pl ./load-chado.yaml biogrid $HOST $DB $USER $PASSWORD 2>&1 | tee -a $LOG_DIR/$log_file.biogrid
 
 echo starting import of GOA GAF data 1>&2
 
@@ -53,19 +55,19 @@ echo $HOME/Work/pombe/pombe-embl/external-go-data/go_proc.tex
 echo $HOME/Work/pombe/pombe-embl/external-go-data/go_func.tex
 ./script/pombase-import.pl ./load-chado.yaml gaf --assigned-by-filter=GeneDB_Spombe $HOST $DB $USER $PASSWORD < $HOME/Work/pombe/pombe-embl/external-go-data/go_func.tex
 echo $HOME/Work/pombe/sources/gene_association.GeneDB_Spombe.inf.gaf
-./script/pombase-import.pl ./load-chado.yaml gaf --term-id-filter-filename=/var/pomcur/sources/pombe-embl/goa-load-fixes/filtered_GO_IDs --with-filter-filename=/var/pomcur/sources/pombe-embl/goa-load-fixes/filtered_mappings --assigned-by-filter=GeneDB_Spombe $HOST $DB $USER $PASSWORD < /var/pomcur/sources/go/scratch/gaf-inference/gene_association.pombase.inf.gaf
+./script/pombase-import.pl ./load-chado.yaml gaf --term-id-filter-filename=$SOURCES/pombe-embl/goa-load-fixes/filtered_GO_IDs --with-filter-filename=$SOURCES/pombe-embl/goa-load-fixes/filtered_mappings --assigned-by-filter=GeneDB_Spombe $HOST $DB $USER $PASSWORD < $SOURCES/go/scratch/gaf-inference/gene_association.pombase.inf.gaf
 echo $HOME/Work/pombe/pombe-embl/external-go-data/From_curation_tool
 ./script/pombase-import.pl ./load-chado.yaml gaf --assigned-by-filter=GeneDB_Spombe $HOST $DB $USER $PASSWORD < $HOME/Work/pombe/pombe-embl/external-go-data/From_curation_tool
 echo $HOME/Work/pombe/pombe-embl/external-go-data/GO_ORFeome_localizations2.tex
 ./script/pombase-import.pl ./load-chado.yaml gaf --assigned-by-filter=GeneDB_Spombe $HOST $DB $USER $PASSWORD < $HOME/Work/pombe/pombe-embl/external-go-data/GO_ORFeome_localizations2.tex
-echo /var/pomcur/sources/gene_association.goa_uniprot.pombe
-./script/pombase-import.pl ./load-chado.yaml gaf --term-id-filter-filename=/var/pomcur/sources/pombe-embl/goa-load-fixes/filtered_GO_IDs --with-filter-filename=/var/pomcur/sources/pombe-embl/goa-load-fixes/filtered_mappings --assigned-by-filter=InterPro,UniProtKB $HOST $DB $USER $PASSWORD < /var/pomcur/sources/gene_association.goa_uniprot.pombe
+echo $SOURCES/gene_association.goa_uniprot.pombe
+./script/pombase-import.pl ./load-chado.yaml gaf --term-id-filter-filename=$SOURCES/pombe-embl/goa-load-fixes/filtered_GO_IDs --with-filter-filename=$SOURCES/pombe-embl/goa-load-fixes/filtered_mappings --assigned-by-filter=InterPro,UniProtKB $HOST $DB $USER $PASSWORD < $SOURCES/gene_association.goa_uniprot.pombe
 
 ) 2>&1 | tee $LOG_DIR/$log_file.gaf
 
 echo load Compara orthologs 1>&2
 
-./script/pombase-import.pl load-chado.yaml orthologs --publication=PMID:19029536 --organism_1_taxonid=4896 --organism_2_taxonid=9606 --swap-direction $HOST $DB $USER $PASSWORD < /var/pomcur/sources/pombe-embl/orthologs/compara_orths.tsv
+./script/pombase-import.pl load-chado.yaml orthologs --publication=PMID:19029536 --organism_1_taxonid=4896 --organism_2_taxonid=9606 --swap-direction $HOST $DB $USER $PASSWORD < $SOURCES/pombe-embl/orthologs/compara_orths.tsv
 
 echo copying $DB to $DB-l1
 createdb -T $DB $DB-l1
