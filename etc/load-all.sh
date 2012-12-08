b719a13b29a6bd2de94e4035838ac2ce352b3d3d
Kim Rutherford <kmr44@cam.ac.uk>
2012-12-08 22:44:49 +1300

    Exit the load script if EMBL load fails

--- etc/load-all.sh
+++ etc/load-all.sh
@@ -30,7 +30,7 @@ $HOME/git/pombase-run/script/load-chado.pl \
   --mapping "pt_mod:PSI-MOD:$HOME/Dropbox/pombase/ontologies/PSI-MOD/modification_map.txt" \
   --mapping "phenotype:fission_yeast_phenotype:$HOME/Dropbox/pombase/ontologies/phenotype/phenotype-map.txt" \
   --obsolete-term-map $HOME/pombe/go-doc/obsoletes-exact $HOME/git/pombase-run/load-chado.yaml \
-  $HOST $DB $USER $PASSWORD $SOURCES/pombe-embl/*.contig 2>&1 | tee $log_file || (echo exiting after failure; exit 1)
+  $HOST $DB $USER $PASSWORD $SOURCES/pombe-embl/*.contig 2>&1 | tee $log_file || {echo exiting after failure; exit 1}
 
 $HOME/git/pombase-run/etc/process-log.pl $log_file
 
