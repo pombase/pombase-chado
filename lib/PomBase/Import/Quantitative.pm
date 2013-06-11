a7740c28371cd57456faab24f87273783a8ce4c1
Kim Rutherford <kmr44@cam.ac.uk>
2013-06-11 14:49:34 +1200

    Revert "Allow copies_per_cell and avg per cell to be null"
    This reverts commit eff8eacc3bc064c0c618a549ea2ff247cce75920.

--- lib/PomBase/Import/Quantitative.pm
+++ lib/PomBase/Import/Quantitative.pm
@@ -158,10 +158,14 @@ method load($fh)
     my $feature_cvterm =
       $self->create_feature_cvterm($feature, $term, $pub, 0);
 
-    $self->add_feature_cvtermprop($feature_cvterm, 'quant_gene_ex_avg_copies_per_cell',
-                                  $average_copies_per_cell);
-    $self->add_feature_cvtermprop($feature_cvterm, 'quant_gene_ex_copies_per_cell',
-                                  $range);
+    if (defined $average_copies_per_cell) {
+      $self->add_feature_cvtermprop($feature_cvterm, 'quant_gene_ex_avg_copies_per_cell',
+                                    $average_copies_per_cell);
+    }
+    if (defined $range) {
+      $self->add_feature_cvtermprop($feature_cvterm, 'quant_gene_ex_copies_per_cell',
+                                    $range);
+    }
     $self->add_feature_cvtermprop($feature_cvterm, 'evidence',
                                   $long_evidence);
     $self->add_feature_cvtermprop($feature_cvterm, 'quant_gene_ex_cell_distribution',
