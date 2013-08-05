pombase/pombase-chado

commit ca9c158fcf424ed7600e63c207babc988eff08aa

Read gene_ex_qualifiers for gene_ex annotations

etc/load-all.sh
@@ -29,6 +29,7 @@ $HOME/git/pombase-run/script/load-chado.pl \
   --mapping "sequence_feature:sequence:$HOME/Dropbox/pombase/ontologies/SO/features-to-so_mapping_only.txt" \
   --mapping "pt_mod:PSI-MOD:$HOME/Dropbox/pombase/ontologies/PSI-MOD/modification_map.txt" \
   --mapping "phenotype:fission_yeast_phenotype:$HOME/Dropbox/pombase/ontologies/phenotype/phenotype-map.txt" \
+  --gene-ex-qualifiers $SOURCES/pombe-embl/supporting_files/gene_ex_qualifiers
   --obsolete-term-map $HOME/pombe/go-doc/obsoletes-exact $HOME/git/pombase-run/load-chado.yaml \
   $HOST $DB $USER $PASSWORD $SOURCES/pombe-embl/*.contig 2>&1 | tee $log_file || exit 1
 

data/chromosome1.contig.embl
@@ -221,7 +221,7 @@ FT                   /controlled_curation="term=protein sequence feature,
 FT                   destruction box; cv=sequence_feature; qualifier=predicted;
 FT                   date=19700101"
 FT                   /controlled_curation="term=gene expression, RNA level;
-FT                   db_xref=PMID:11376151;"
+FT                   db_xref=PMID:11376151; qualifier=increased"
 FT   3'UTR           5271..5369
 FT                   /note="mRNA from AU013226"
 FT                   /systematic_id="SPAC977.11"
@@ -304,8 +304,8 @@ FT                   cv=sequence_feature; date=20060605"
 FT                   /controlled_curation="term=protein sequence feature,
 FT                   coiled-coil; qualifier=region; cv=sequence_feature;
 FT                   date=20060605"
-FT                   /controlled_curation="term=gene expression, alternative
-FT                   transcripts; db_xref=PMID:11376151; cv=gene_ex;
+FT                   /controlled_curation="term=gene expression, translation;
+FT                   db_xref=PMID:11376151; cv=gene_ex;
 FT                   annotation_extension=has_downstream_target(GeneDB_Spombe:SPAC977.10)|has_downstream_target(GeneDB_Spombe:SPBC409.20c);
 FT                   date=20060605"
 FT                   /controlled_curation="term=species distribution, sequence
@@ -320,8 +320,8 @@ FT                   /controlled_curation="term=name description, Meiotic
 FT                   Expression Upregulated; cv=name_description;
 FT                   db_xref=PMID:11376151; date=19700101"
 FT                   /controlled_curation="term=XP-G family; date=19700101"
-FT                   /controlled_curation="term=gene expression, expressed
-FT                   during meiosis; cv=gene_ex; db_xref=PMID:11376151;
+FT                   /controlled_curation="term=gene expression, RNA degradation;
+FT                   cv=gene_ex; db_xref=PMID:11376151;
 FT                   date=20100103"
 FT                   /shared_id="SPAC1556.06"
 FT                   /controlled_curation="term=DNA binding specificity, binds
@@ -350,8 +350,8 @@ FT                   transcripts; cv=gene_ex; db_xref=PMID:11376151;
 FT                   date=20060605"
 FT                   /controlled_curation="term=species distribution, sequence
 FT                   orphan; date=19700101"
-FT                   /controlled_curation="term=gene expression, expressed
-FT                   during meiosis; cv=gene_ex; db_xref=PMID:11376151;
+FT                   /controlled_curation="term=gene expression, protein level;
+FT                   cv=gene_ex; db_xref=PMID:11376151;
 FT                   date=20100103"
 FT                   /controlled_curation="term=species distribution, sequence
 FT                   orphan, uncharacterised; date=20081110"
@@ -362,8 +362,8 @@ FT                   apparent orthologs; date=20090303"
 FT                   /controlled_curation="term=name description, Meiotic
 FT                   Expression Upregulated; cv=name_description;
 FT                   db_xref=PMID:11376151; date=19700101"
-FT                   /controlled_curation="term=gene expression, expressed
-FT                   during meiosis; cv=gene_ex; db_xref=PMID:11376151;
+FT                   /controlled_curation="term=gene expression, transcription;
+FT                   cv=gene_ex; db_xref=PMID:11376151;
 FT                   date=20100103"
 FT   intron          4588..4660
 FT                   /note="intron confirmed by mRNA"

lib/PomBase/Chado/QualifierLoad.pm
@@ -60,6 +60,18 @@ with 'PomBase::Role::PhenotypeFeatureFinder';
 
 has verbose => (is => 'ro', isa => 'Bool');
 
+has gene_ex_qualifiers => (is => 'ro', init_arg => undef,
+                           lazy_build => 1);
+
+method _build_gene_ex_qualifiers
+{
+  my @gene_ex_qualifiers = @{$self->config()->{gene_ex_qualifiers}};
+
+  my %gene_ex_qualifiers = map { ($_, 1) } @gene_ex_qualifiers;
+
+  return \%gene_ex_qualifiers;
+}
+
 method find_cv_by_name($cv_name) {
   die 'no $cv_name' unless defined $cv_name;
 
@@ -752,6 +764,33 @@ method process_family($chado_object, $term, $sub_qual_map)
   return 1;
 }
 
+method check_gene_ex_quals($term, $sub_qual_map)
+{
+  my %gene_ex_term_names = ('RNA level' => 1,
+                            'protein level' => 1,
+                            'transcription' => 1,
+                            'translation' => 1,
+                            'RNA degradation' => 1,
+                            'protein degradation' => 1,
+                          );
+
+  if (!exists $gene_ex_term_names{$term}) {
+    die qq(gene expression cv has no "$term" term\n);
+  }
+
+  my $qualifier = $sub_qual_map->{qualifier};
+
+  if (!defined $qualifier) {
+    die qq(gene expression annotations must have a "qualifier=" sub-qualifier\n);
+  }
+
+  my $qual_val = $qualifier->[0];
+
+  if (!exists $self->gene_ex_qualifiers()->{$qual_val}) {
+    die qq("$qual_val" is not a valid qualifier for gene expression annotation\n);
+  }
+}
+
 method process_one_cc($chado_object, $bioperl_feature, $qualifier,
                       $target_curations) {
   my $systematic_id = $chado_object->uniquename();
@@ -846,6 +885,9 @@ method process_one_cc($chado_object, $bioperl_feature, $qualifier,
       try {
         if (defined $qual_map{qualifier}) {
           $self->maybe_move_predicted($qual_map{qualifier}, \%qual_map);
+        }
+        if ($cv_name eq 'gene_ex') {
+          $self->check_gene_ex_quals($term, \%qual_map);
         }
         $self->add_term_to_gene($chado_object, $cv_name, $term, \%qual_map, 1);
       } catch {

script/load-chado.pl
@@ -26,6 +26,7 @@ my $quiet = 0;
 my $dry_run = 0;
 my $test = 0;
 my @obsolete_term_mapping_files = ();
+my $gene_ex_qualifiers;
 my @mappings = ();
 
 sub usage {
@@ -37,6 +38,7 @@ if (!GetOptions("verbose|v" => \$verbose,
                 "quiet|q" => \$quiet,
                 "test|t" => \$test,
                 "obsolete-term-map=s" => \@obsolete_term_mapping_files,
+                "gene-ex-qualifiers=s" => \$gene_ex_qualifiers,
                 "mapping|m=s" => \@mappings)) {
   usage();
 }
@@ -123,6 +125,28 @@ $config->{obsolete_term_mapping} = {
 };
 
 $config->{target_quals} = {};
+
+func read_gene_ex_qualifiers($gene_ex_qualifiers) {
+  open my $fh, '<', $gene_ex_qualifiers
+    or die "can't opn $gene_ex_qualifiers: $!";
+
+  my @ret_val = ();
+
+  while (defined (my $line = <$fh>)) {
+    next if $line =~ /^!/;
+
+    chomp $line;
+
+    push @ret_val, $line;
+  }
+
+  close $fh;
+
+  return \@ret_val;
+}
+
+$config->{gene_ex_qualifiers} =
+  read_gene_ex_qualifiers($gene_ex_qualifiers);
 
 for my $allowed_unknown_term_names_file (@{$config->{allowed_unknown_term_names_files}}) {
   open my $unknown_names, '<', $allowed_unknown_term_names_file or die;

commit 75bfdd81cb3d26a2ac1f86af05162dd22189358c

Fix syntax of load script caused by [ca9c158fc]

etc/load-all.sh
@@ -29,7 +29,7 @@ $HOME/git/pombase-run/script/load-chado.pl \
   --mapping "sequence_feature:sequence:$HOME/Dropbox/pombase/ontologies/SO/features-to-so_mapping_only.txt" \
   --mapping "pt_mod:PSI-MOD:$HOME/Dropbox/pombase/ontologies/PSI-MOD/modification_map.txt" \
   --mapping "phenotype:fission_yeast_phenotype:$HOME/Dropbox/pombase/ontologies/phenotype/phenotype-map.txt" \
-  --gene-ex-qualifiers $SOURCES/pombe-embl/supporting_files/gene_ex_qualifiers
+  --gene-ex-qualifiers $SOURCES/pombe-embl/supporting_files/gene_ex_qualifiers \
   --obsolete-term-map $HOME/pombe/go-doc/obsoletes-exact $HOME/git/pombase-run/load-chado.yaml \
   $HOST $DB $USER $PASSWORD $SOURCES/pombe-embl/*.contig 2>&1 | tee $log_file || exit 1
 