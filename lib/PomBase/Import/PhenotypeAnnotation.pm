653d402c9cb5e1ad3ca778e71347ea451e6f9873
Kim Rutherford <kmr44@cam.ac.uk>
2013-09-27 17:35:00 +1200

    Gene names in PHAF that are uniquenames are OK

--- lib/PomBase/Import/PhenotypeAnnotation.pm
+++ lib/PomBase/Import/PhenotypeAnnotation.pm
@@ -169,7 +169,8 @@ method load($fh)
       my $gene_uniquename = $gene->uniquename();
       my $existing_gene_name = $gene->name() // '';
 
-      if (length $gene_name > 0 && $gene_name ne $existing_gene_name) {
+      if (length $gene_name > 0 && $gene_name ne $existing_gene_name &&
+          $gene_name ne $gene_uniquename) {
         warn qq|gene name from phenotype annotation file ("$gene_name") doesn't | .
           qq|match the existing name ("$existing_gene_name") for $gene_uniquename | .
           qq|- skipping|;
