05ab252ecc2d98ab5f36bf373f3d1409c929e658
Kim Rutherford <kmr44@cam.ac.uk>
2013-10-06 16:58:58 +1300

    Add missing file to the log directory

--- etc/load-all.sh
+++ etc/load-all.sh
@@ -170,7 +170,7 @@ cp $LOG_DIR/$log_file.manual_multi_orths $DUMP_DIR/logs/$log_file.manual-multi-o
 cp $LOG_DIR/$log_file.manual_1-1_orths $DUMP_DIR/logs/$log_file.manual-1-1-orths-output
 cp $LOG_DIR/$log_file.curation_tool_data $DUMP_DIR/logs/$log_file.curation-tool-data-load-output
 cp $LOG_DIR/$log_file.quantitative $DUMP_DIR/logs/$log_file.quantitative
-cp $LOG_DIR/$log_file.phenotypes_from_* $DUMP_DIR/logs/
+cp $LOG_DIR/$log_file.*phenotypes_from_* $DUMP_DIR/logs/
 
 psql $FINAL_DB -c "select count(id), name from (select p.cvterm_id::text || '_cvterm' as id,
  substring(type.name from 'annotation_extension_relation-(.*)') as name from
