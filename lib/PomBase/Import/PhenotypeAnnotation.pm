4c86160f6c5adfc6415072361da68e9c24b8a7fd
Kim Rutherford <kmr44@cam.ac.uk>
2014-10-21 09:56:57 +1300

    Remove unneeded code that was causing a warning

--- lib/PomBase/Import/PhenotypeAnnotation.pm
+++ lib/PomBase/Import/PhenotypeAnnotation.pm
@@ -89,8 +89,6 @@ method load($fh)
                         reference taxon date illegal_extra_column));
 
   while (my $columns_ref = $csv->getline_hr($fh)) {
-    $csv->is_missing (0) and next; # This was an empty line
-
     my $gene_systemtic_id = $columns_ref->{"gene_systemtic_id"}->trim();
 
     if ($gene_systemtic_id =~ /^#/ ||
