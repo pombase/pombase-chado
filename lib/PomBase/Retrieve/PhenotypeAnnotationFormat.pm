pombase/pombase-chado

commit 8899a927cb7eff1f7911f44a65e9655553ee2863

Fix warnings from PHAF output
Refs #470

lib/PomBase/Retrieve/PhenotypeAnnotationFormat.pm
@@ -309,9 +309,10 @@ method retrieve() {
         {
           'me.cvterm_id' => { -in => $cvterm_rs->get_column('cvterm_id')->as_query() },
           'feature.organism_id' => $self->organism()->organism_id(),
+          'type.name' => 'genotype',
         },
         {
-          join => 'feature',
+          join => { 'feature' => 'type' },
         });
 
   my $genotype_rs = $feature_cvterm_rs->search_related('feature');
@@ -342,8 +343,6 @@ method retrieve() {
 
       if (defined $row) {
         my ($extensions, $base_cvterm) = $self->make_gaf_extension($row);
-
-        $extensions =~ s/(has_penetrance|has_expressivity)\([^\)]+\)//;
 
         my $fc_id = $row->feature_cvterm_id();
         my %row_fc_props = %{$fc_props{$fc_id}};

commit 88181060cd5bd9ab64fd5e4ba9e54e442aa40abb

Fix test broken by 8899a927cb7eff1f7911
Accidentally removed some useful code.

lib/PomBase/Retrieve/PhenotypeAnnotationFormat.pm
@@ -344,6 +344,9 @@ method retrieve() {
       if (defined $row) {
         my ($extensions, $base_cvterm) = $self->make_gaf_extension($row);
 
+        # we have columns for these:
+        $extensions =~ s/(has_penetrance|has_expressivity)\([^\)]+\)//;
+
         my $fc_id = $row->feature_cvterm_id();
         my %row_fc_props = %{$fc_props{$fc_id}};
         my $cvterm = $base_cvterm // $row->cvterm();