de11ac3c271dbeb4a93e39d3ae27ba2d806296ea
Kim Rutherford <kmr44@cam.ac.uk>
2016-07-27 14:47:25 +1200

    Store new pubprops from Canto
    See pombase/canto@fcd5ae62349c7c47d65e51447063719883073bb3

--- lib/PomBase/Import/Canto.pm
+++ lib/PomBase/Import/Canto.pm
@@ -773,6 +773,8 @@ method _store_metadata($metadata) {
   $self->create_pubprop($pub, 'canto_approver_email', $metadata->{approver_email});
   $self->create_pubprop($pub, 'canto_curator_name', $metadata->{curator_name});
   $self->create_pubprop($pub, 'canto_curator_email', $metadata->{curator_email});
+  $self->create_pubprop($pub, 'canto_curator_role', $metadata->{curator_role});
+  $self->create_pubprop($pub, 'canto_session_accepted_date', $metadata->{curation_accepted_date});
   if ($metadata->{message_for_curators}) {
     $self->create_pubprop($pub, 'canto_message_for_curators', $metadata->{message_for_curators});
   }
