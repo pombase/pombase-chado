4f9460a06d605d03dd6d025b5642afd7ab66b050
Kim Rutherford <kmr44@cam.ac.uk>
2017-04-30 16:49:08 +1200

    Add SequenceTranslator package
    Refs pombase/website#234

---
+++ lib/PomBase/Role/SequenceTranslator.pm
@@ -0,0 +1,56 @@
+package PomBase::Role::SequenceTranslator;
+
+=head1 NAME
+
+PomBase::Role::SequenceTranslator - a role for calling BioPerl to translate
+                                    an mRNA sequence
+
+=head1 SYNOPSIS
+
+=head1 AUTHOR
+
+Kim Rutherford C<< <kmr44@cam.ac.uk> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<kmr44@cam.ac.uk>.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc PomBase::Role::SequenceTranslator
+
+=over 4
+
+=back
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2013 Kim Rutherford, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=head1 FUNCTIONS
+
+=cut
+
+use perl5i::2;
+
+use Bio::Seq;
+use Bio::Tools::SeqStats;
+
+use Moose::Role;
+
+method translate_sequence($residues, $phase) {
+  my $seq = Bio::Seq->new(-seq => $residues, -alphabet => 'dna');
+
+  my $prot_seq = $seq->translate();
+  my $seq_stats = Bio::Tools::SeqStats->new(-seq => $prot_seq);
+  my $weight = $seq_stats->get_mol_wt();
+
+  return ($prot_seq->seq(), $weight->[0]);
+}
+
+1;
