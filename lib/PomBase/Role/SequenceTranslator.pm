89ab8196f1a8074bbc79b3e48cdc7b29cc92f42d
Kim Rutherford <kmr44@cam.ac.uk>
2017-05-26 14:51:01 +1200

    Add more protein properties
    Refs pombase/website#96

--- lib/PomBase/Role/SequenceTranslator.pm
+++ lib/PomBase/Role/SequenceTranslator.pm
@@ -40,17 +40,43 @@ use perl5i::2;
 
 use Bio::Seq;
 use Bio::Tools::SeqStats;
+use Bio::Tools::pICalculator;
+use Bio::Tools::CodonOptTable;
 
 use Moose::Role;
 
 method translate_sequence($residues, $phase, $is_mito) {
   my $seq = Bio::Seq->new(-seq => $residues, -alphabet => 'dna');
 
-  my $prot_seq = $seq->translate(undef, undef, $phase, $is_mito ? 4 : 1);
-  my $seq_stats = Bio::Tools::SeqStats->new(-seq => $prot_seq);
-  my $weight = $seq_stats->get_mol_wt();
+  my $genetic_code = $is_mito ? 4 : 1;
 
-  return ($prot_seq->seq(), $weight->[0]);
+  my $prot_seq = $seq->translate(undef, undef, $phase, $genetic_code);
+  my $seq_stats = Bio::Tools::SeqStats->new(-seq => $prot_seq);
+  my $number_of_residues = length($prot_seq->seq());
+  my $weight = $seq_stats->get_mol_wt();
+  my $pIcalc = Bio::Tools::pICalculator->new(-places => 2);
+  $pIcalc->seq($prot_seq);
+  my $charge_at_ph7 = $pIcalc->charge_at_pH(7);
+  my $isoelectric_point = $pIcalc->iep();
+
+  my $cai_seqobj = Bio::Tools::CodonOptTable->new(
+    -seq              => $residues,
+    -alphabet         => 'dna',
+    -is_circular      => 0,
+    -genetic_code     => $genetic_code,
+  );
+
+  my $codon_adaptation_index = $cai_seqobj->calculate_cai($cai_seqobj->rscu_rac_table());
+
+  my $stats = {
+    molecular_weight => $weight->[0],
+    average_residue_weight => int(100 * $weight->[0] / $number_of_residues) / 100.0,
+    charge_at_ph7 => int($charge_at_ph7 * 100) / 100.0,
+    codon_adaptation_index => $codon_adaptation_index,
+    isoelectric_point => $isoelectric_point,
+  };
+
+  return ($prot_seq->seq(), $stats);
 }
 
 1;
