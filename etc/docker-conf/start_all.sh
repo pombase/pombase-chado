0697710537a1635a7212678cf4c5f5f5ca4b9c46
Kim Rutherford <kmr44@cam.ac.uk>
2017-11-06 17:19:15 +1300

    Exec circus so that control-C kills "docker run"

--- etc/docker-conf/start_all.sh
+++ etc/docker-conf/start_all.sh
@@ -3,7 +3,8 @@
 if [ $ANALYTICS_ID = UNSET ]
 then
   echo "ANALYTICS_ID is not set - container won't start"
+  exit 1
 else
   /pombase/update_analytics_id.sh $ANALYTICS_ID /var/www/html/index.html &&
-  /usr/local/bin/circusd /pombase/circus.ini
+  exec /usr/local/bin/circusd /pombase/circus.ini
 fi
