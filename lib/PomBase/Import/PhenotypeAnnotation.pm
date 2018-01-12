a0c2875a386559ad9c34fb1cb8ee89cb8979be8f
Kim Rutherford <kmr44@cam.ac.uk>
2018-01-12 15:04:58 +1300

    Be more relaxed about PHAF header parsing

--- lib/PomBase/Import/PhenotypeAnnotation.pm
+++ lib/PomBase/Import/PhenotypeAnnotation.pm
@@ -155,7 +155,7 @@ method load($fh) {
     my $gene_systemtic_id = $columns_ref->{"gene_systemtic_id"}->trim();
 
     if ($gene_systemtic_id =~ /^#/ ||
-        ($fh->input_line_number() == 1 && $gene_systemtic_id =~ /Gene .*ID/)) {
+        ($fh->input_line_number() == 1 && $gene_systemtic_id =~ /Gene .*ID|systematic/)) {
       next;
     }
 
