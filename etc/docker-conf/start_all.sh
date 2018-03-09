d9b68418ce08c084fe74a362e045fc3b5b6efad1
Kim Rutherford <kmr44@cam.ac.uk>
2018-03-09 15:39:17 +1300

    Set window.pombaseAppDeployConfig in app

--- etc/docker-conf/start_all.sh
+++ etc/docker-conf/start_all.sh
@@ -1,10 +1,15 @@
 #!/bin/bash -
 
+if [ x$APP_DEPLOY_CONFIG = x ]
+then
+    APP_DEPLOY_CONFIG="{ mode: 'prod' }"
+fi
+
 if [ $ANALYTICS_ID = UNSET ]
 then
   echo "ANALYTICS_ID is not set - container won't start"
   exit 1
 else
-  /pombase/update_analytics_id.sh $ANALYTICS_ID /var/www/html/index.html &&
+  /pombase/update_envs.sh $ANALYTICS_ID "$APP_DEPLOY_CONFIG" /var/www/html/index.html &&
   exec /usr/local/bin/circusd /pombase/circus.ini
 fi
