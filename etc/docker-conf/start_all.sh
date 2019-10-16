2def17873b978dca42cf6d8060eea42bf600a645
Kim Rutherford <kmr@aska.gen.nz>
2019-10-16 20:51:30 +1300

    Fix host address in Postgresql connect string
    Refs #26
    Refs #585

--- etc/docker-conf/start_all.sh
+++ etc/docker-conf/start_all.sh
@@ -10,6 +10,8 @@ then
   echo "ANALYTICS_ID is not set - container won't start"
   exit 1
 else
+  export HOST_IP_ADDRESS=$(route | grep '^default' | awk '{print $2}')
+
   /pombase/update_vars.sh $ANALYTICS_ID "$APP_DEPLOY_CONFIG" /var/www/html/index.html &&
   exec /usr/local/bin/circusd /pombase/circus.ini
 fi
