pombase/pombase-chado

commit 8f3e96b50d6bd98c8e33acbf5ac4bc962d132d21

Give up on perl5i which doesn't work on newer Perl

lib/PomBase/Check.pm
@@ -35,7 +35,10 @@ under the same terms as Perl itself.
 
 =cut
 
-use perl5i::2;
+use strict;
+use warnings;
+use Carp;
+
 use Moose;
 use feature qw(switch);
 no if $] >= 5.018, warnings => "experimental::smartmatch";
@@ -49,7 +52,9 @@ with 'PomBase::Role::ChadoUser';
 
 has website_config => (is => 'ro');
 
-method _do_query_checks() {
+sub _do_query_checks {
+  my $self = shift;
+
   my @query_checks = @{$self->config()->{check_chado}->{query_checks}};
 
   my $dbh = $self->chado()->storage()->dbh();
@@ -141,7 +146,9 @@ method _do_query_checks() {
   return $seen_failure;
 }
 
-method run() {
+sub run {
+  my $self = shift;
+
   my $seen_failure = 0;
 
   my @check_modules = usesub PomBase::Check;

lib/PomBase/Check/PhenotypesNotInCategory.pm
@@ -37,17 +37,24 @@ under the same terms as Perl itself.
 
 =cut
 
-use perl5i::2;
+use strict;
+use warnings;
+use Carp;
+
 use Moose;
 
-method description() {
+sub description {
+  my $self = shift;
+
   return "Check that all phenotype terms have a parent in one of the categories " .
     "in the website configuration for fission_yeast_phenotype";
 }
 
 with 'PomBase::Checker';
 
-method check() {
+sub check {
+  my $self = shift;
+
   my $chado = $self->chado();
 
   my $fypo_config = $self->website_config()->{cv_config}->{fission_yeast_phenotype};

commit b6469d57f0773abb8bd938272882861a7f375193

Fix Chado checks, broken by 8f3e96b50d6b
See also: https://github.com/pombase/pombase-chado/issues/831

lib/PomBase/Check.pm
@@ -40,7 +40,7 @@ use warnings;
 use Carp;
 
 use Moose;
-use feature qw(switch);
+use feature qw(switch say);
 no if $] >= 5.018, warnings => "experimental::smartmatch";
 
 use Module::Find;

lib/PomBase/Check/PhenotypesNotInCategory.pm
@@ -43,6 +43,8 @@ use Carp;
 
 use Moose;
 
+use feature qw(say);
+
 sub description {
   my $self = shift;
 