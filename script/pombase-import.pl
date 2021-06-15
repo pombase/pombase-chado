68d55bc117a33d2091884f99b679d06c331e3fdf
Kim Rutherford <kmr@aska.gen.nz>
2021-06-15 23:12:49 +1200

    Fix typo in flag for name and product importer
    Refs japonicusdb/japonicus-config#12

--- script/pombase-import.pl
+++ script/pombase-import.pl
@@ -34,7 +34,7 @@ my %input_types = (
   'phenotype-annotation' => 'PomBase::Import::PhenotypeAnnotation',
   'references-file' => 'PomBase::Import::ReferencesFile',
   'kegg-pathway' => 'PomBase::Import::KEGGMapping',
-  'name-and-product' => 'PomBase::Import::NameAndProduct',
+  'names-and-products' => 'PomBase::Import::NameAndProduct',
 );
 
 sub usage
