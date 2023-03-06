58a1ca0dcb0d4d0e833262df6b02ef2ab301c5c4
Kim Rutherford <kmr@aska.gen.nz>
2023-03-06 19:02:27 +1300

    Add Google Tag Manager ID
    Refs pombase/website#1870

--- etc/docker-conf/start_all.sh
+++ etc/docker-conf/start_all.sh
@@ -9,10 +9,16 @@ if [ $ANALYTICS_ID = UNSET ]
 then
   echo "ANALYTICS_ID is not set - container won't start"
   exit 1
-else
-  export HOST_IP_ADDRESS=$(route | grep '^default' | awk '{print $2}')
-
-  /pombase/update_vars.sh $ANALYTICS_ID "$APP_DEPLOY_CONFIG" /var/www/html/index.html &&
-  perl -pne "s/<<database_name>>/$DATABASE_NAME/g" /pombase/circus.ini.template > /pombase/circus.ini
-  exec /usr/local/bin/circusd --log-level debug /pombase/circus.ini
 fi
+
+if [ $GOOGLE_TAG_MANAGER_ID = UNSET ]
+then
+  echo "GOOGLE_TAG_MANAGER_ID is not set - container won't start"
+  exit 1
+fi
+
+export HOST_IP_ADDRESS=$(route | grep '^default' | awk '{print $2}')
+
+/pombase/update_vars.sh $ANALYTICS_ID $GOOGLE_TAG_MANAGER_ID "$APP_DEPLOY_CONFIG" /var/www/html/index.html &&
+perl -pne "s/<<database_name>>/$DATABASE_NAME/g" /pombase/circus.ini.template > /pombase/circus.ini
+exec /usr/local/bin/circusd --log-level debug /pombase/circus.ini
