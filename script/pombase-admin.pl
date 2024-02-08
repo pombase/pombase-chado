768c2010ab015f1da0e8a3a8e9328da92edf986e
Kim Rutherford <kmr@aska.gen.nz>
2024-02-08 21:35:12 +1300

    Add "add-chado-prop" action to admin script
    Refs pombase/website#1927

--- script/pombase-admin.pl
+++ script/pombase-admin.pl
@@ -24,13 +24,21 @@ usage:
 
 Six arguments are always required:
   config_file   - the YAML format configuration file name
-  action        - currently must be "chado-init" which adds initial cvterms
-                  and dbxrefs for feature loading using the "cvs" section of
-                  the config file
+  action        - see below
   host          - the database server machine name
   database_name - the Chado database name
   username      - the database user name
   password      - the database password
+
+Actions:
+
+  chado-init     - needs no extra arguments; adds initial cvterms an
+                   dbxrefs for feature loading using the "cvs" section of
+                   the config file
+
+  add-chado-prop - add a value to the chadoprop table using an existing cvterm;
+                   needs two extra arguments: prop_type_name and prop_value
+                   eg. "goa_version" and "218"
 );
 
 }
@@ -56,11 +64,6 @@ my $database = shift;
 my $username = shift;
 my $password = shift;
 
-if (@ARGV) {
-  warn "too many arguments\n";
-  usage();
-}
-
 my $chado = PomBase::Chado::db_connect($host, $database, $username, $password);
 
 my $guard = $chado->txn_scope_guard;
@@ -73,6 +76,27 @@ $config->{id_counter} = $id_counter;
 
 if ($action eq 'chado-init') {
   PomBase::Chado::InitUtil::init_objects($chado, $config);
+}
+
+if ($action eq 'add-chado-prop') {
+  if (@ARGV < 2) {
+    warn "not enough arguments\n";
+    usage();
+  }
+
+  my $prop_name = shift;
+  my $prop_value = shift;
+
+  my $type_cvterm = $chado->resultset('Cv::Cvterm')
+    ->find({ name => $prop_name });
+
+  if (!defined $type_cvterm) {
+    die "can't find cvterm for $prop_name\n";
+  }
+
+  $chado->resultset('Cv::Chadoprop')
+    ->create({ type_id => $type_cvterm->cvterm_id(),
+               value => $prop_value });
 }
 
 $guard->commit unless $dry_run;
