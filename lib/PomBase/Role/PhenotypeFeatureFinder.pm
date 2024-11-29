3db394e2758905e1e3c0554159fe3d2fef460210
Kim Rutherford <kmr@aska.gen.nz>
2024-11-29 13:58:27 +1300

    Fix WT names that include systematic IDs
    Only for genes that have names.
    
    For example, if an incoming WT allele name is "SPCC417.09c+", change
    it to "sdr1+".
    
    Same for deletions, change "SPCC417.09cdelta" to "sdr1delta".
    
    Refs pombase/pombase-chado#1242

--- lib/PomBase/Role/PhenotypeFeatureFinder.pm
+++ lib/PomBase/Role/PhenotypeFeatureFinder.pm
@@ -552,10 +552,12 @@ sub get_allele {
         Dumper([$allele_data]);
     }
 
-    if ($new_allele_type eq 'wild_type' && !$new_allele_name) {
+    if ($new_allele_type eq 'wild_type' &&
+        (!$new_allele_name || $new_allele_name eq "$gene_uniquename+")) {
       $new_allele_name = ($gene_name || $gene_uniquename) . '+';
     }
-    if($new_allele_type eq 'deletion' && !$new_allele_name) {
+    if($new_allele_type eq 'deletion' &&
+       (!$new_allele_name || $new_allele_name eq "${gene_uniquename}delta")) {
       $new_allele_name = ($gene_name || $gene_uniquename) . 'delta';
     }
 
