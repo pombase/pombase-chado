19e74d5d71c6eecd7cfa768436286c9e6efe57ee
Kim Rutherford <kmr@aska.gen.nz>
2025-02-06 19:36:35 +1300

    Copy PB_references.txt to the container
    Refs pombase/website#2325

--- etc/build_container.sh
+++ etc/build_container.sh
@@ -41,6 +41,7 @@ rsync -acvPHS --delete-after $dump_dir/fasta/chromosomes/ $container_dir/chromos
 rsync -acvPHS --delete-after $dump_dir/fasta/bgzip_chromosomes $container_dir/
 
 cp $dump_dir/api_maps.sqlite3.zst $container_dir/
+cp $dump_dir/pombe-embl/supporting_files/PB_references.txt $container_dir/
 
 mkdir -p $container_dir/feature_sequences
 rsync -acvPHS --delete-after $dump_dir/fasta/feature_sequences/peptide.fa.gz $container_dir/feature_sequences/peptide.fa.gz
