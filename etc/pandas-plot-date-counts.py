f2b643959ac3b1b1c827f993448f6fc0cccb701c
Kim Rutherford <kmr@aska.gen.nz>
2025-09-25 23:30:18 +1200

    Label y-axis, remove legend of GO-CAM graphs
    Refs pombase/website#2324

--- etc/pandas-plot-date-counts.py
+++ etc/pandas-plot-date-counts.py
@@ -12,8 +12,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 type = sys.argv[1]
-file = sys.argv[2]
-out = sys.argv[3]
+y_label = sys.argv[2]
+file = sys.argv[3]
+out = sys.argv[4]
 
 df = pd.read_csv(file)
 
@@ -23,11 +24,14 @@ default_dpi=600
 figsize=(8,5)
 fig, ax = plt.subplots(dpi=default_dpi, figsize=figsize)
 
-ax = df.plot(ax=ax, kind='line', x='date', y=type + 's')
+ax = df.plot(ax=ax, legend=False, kind='line', x='date', y=type + 's')
 
 plt.subplots_adjust(bottom=0.18)
 plt.grid(color='0.85')
 
 ax.tick_params(axis='x', labelrotation = 45)
+
+plt.xlabel('Date')
+plt.ylabel(y_label)
 
 plt.savefig(out)
